b4227c7c9ed13e0a2a8a0d2390173192b2f2b2e1
yusufdani2803 <yusufdani2803@gmail.com>
2019-03-08 15:20:08 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,68 +36,30 @@
     <div class="limiter">
         <div class="container-login100">
             <div class="wrap-login100">
-                <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
-                     {{ csrf_field() }}
+                <form class="login100-form validate-form">
                     <span class="login100-form-title p-b-26">
                         Welcome
                     </span>
                     <span class="login100-form-title p-b-48">
-                        <img src="{{asset('admin/assets/img/th.jpeg')}}" width="150" class="img-circle" alt="Avatar">
+                        <i class="zmdi zmdi-font"></i>
                     </span>
                 <div class="panel-body">
+                    <form  method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
 
                         <div class="form-group">
 
                             <div class="wrap-input100 validate-input">
-                                <input id="email" type="email" class="input100" name="email" value="{{ old('email') }}" >
+                                <input id="email" type="email" class="input100" name="email" value="{{ old('email') }}">
                                 <span class="focus-input100" data-placeholder="Email"></span>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
-=======
-    <title>Persuratan</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->  
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
-    
-    <div class="limiter">
-        <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
-            <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-                <form class="login100-form validate-form"method="POST" action="{{ route('login') }}">
-                     {{ csrf_field() }}
-                    <span class="login100-form-title p-b-49">
-                        Login
-                    </span>
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-                    <div class="wrap-input100 validate-input m-b-23" data-validate = "email">
-
-                        <span class="label-input100" id="email" type="email"  name="email" value="{{ old('email') }}">Email</span>
-                        <input class="input100" type="email" name="email" placeholder="email">
-                        <span class="focus-input100" data-symbol="&#xf206;"></span>
-                        @if ($errors->has('email'))
-                        <span class="help-block">
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 
                             <div class="wrap-input100 validate-input">
@@ -130,6 +92,7 @@
                             Sign Up
                         </a>
                     </div>
+                    </form>
                 </div>
 </form>
 </div>
@@ -158,59 +121,6 @@
   <script src="vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
   <script src="js/main.js"></script>
-=======
-                    <div class="txt1 text-center p-t-54 p-b-20">
-                        <span>
-                            Or Sign Up Using
-                        </span>
-                    </div>
-
-                    <div class="flex-c-m">
-                        <a href="#" class="login100-social-item bg1">
-                            <i class="fa fa-facebook"></i>
-                        </a>
-
-                        <a href="#" class="login100-social-item bg2">
-                            <i class="fa fa-twitter"></i>
-                        </a>
-
-                        <a href="#" class="login100-social-item bg3">
-                            <i class="fa fa-google"></i>
-                        </a>
-                    </div>
-
-                    <div class="flex-col-c p-t-155">
-                        <span class="txt1 p-b-17">
-                            Or Sign Up Using
-                        </span>
-
-                        <a href="#" class="txt2">
-                            Sign Up
-                        </a>
-                    </div>
-            </div>
-        </div>
-    </div>
-    
-
-    <div id="dropDownSelect1"></div>
-    
-<!--===============================================================================================-->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/daterangepicker/moment.min.js"></script>
-    <script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-    <script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-    <script src="js/main.js"></script>
 
 </body>
 </html>
